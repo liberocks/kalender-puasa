93cdc8c81e31ece851765976971f1ba0604f214f
liberocks <tirtadwipa.manunggal@gmail.com>
2024-06-15 17:09:13 +0700

    fix overflow issue

--- components/container.tsx
+++ components/container.tsx
@@ -1,6 +1,6 @@
 export const Container: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="relative mx-auto flex h-[100%] min-h-[100vh] max-w-2xl flex-col bg-[#F6F4F0] px-4 sm:px-6 lg:px-8">
+    <div className="relative mx-auto flex h-[100%] min-h-[100vh] max-w-2xl flex-col overflow-x-hidden bg-[#F6F4F0] px-4 sm:px-6 lg:px-8">
       {children}
     </div>
   );
